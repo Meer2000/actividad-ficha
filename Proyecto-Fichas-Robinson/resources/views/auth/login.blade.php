c619b06fd67e938db638cd1f305617156637b8ee
Camilo_Dev.JR <juan.camilorodrigez@gmail.com>
2023-02-08 23:12:55 -0500

    Agregando todo recursos, tablas y vistas JC

--- Proyecto-Fichas-Robinson/resources/views/auth/login.blade.php
+++ Proyecto-Fichas-Robinson/resources/views/auth/login.blade.php
@@ -1,48 +1,67 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.guest')
+@section('title', 'Iniciar Sesión')
+@section('content')
+    <header class="headers" style="">
+        <div class="inner-top d-flex justify-content-center justify-content-sm-center justify-content-md-between justify-content-lg-between" >
+                <a href="{{url('auth.login')}}" id="logo-sena" class="img-fluid" title="Logo Sena"></a>
 
-        <x-jet-validation-errors class="mb-4" />
+                <div class="d-none d-sm-none d-md-block">
+                    <input class="img-fluid " type="image" style="height: 127px; margin-top: -12px; " name="imgbtn" src="{{url('../../recursos/img/login-via-microsoft.png')}}" id="">
+                </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+
+        </div>
+    </header>
+    <div class="linea"></div>
+    <main style="height: 75vh">
+        <div id="content" style="background-color: #ffffff">
+            <div class="inner-mid d-flex align-items-center justify-content-center">
+                <div class="container col-sm-12 col-lg-5 form-container-login">
+                    <form action="">
+                        <div class="" style="background: white;">
+                            <div class="title-content mb-4">
+                                <p>Ingreso Usuarios registrados</p>
+                            </div>
+                            <div class="content-login">
+                                <div class="form-floating  mb-3 ">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                                    <label for="floatingInput">Correo Electronico</label>
+                                </div>
+                                <div class="form-floating ">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña" required>
+                                    <label for="floatingPassword">Contraseña</label>
+                                </div>
+                                <div>
+                                    <p><i></i>¿Olvidé mi contraseña?</p>
+                                </div>
+                                <div class="d-flex justify-content-center mt-3 mb-3">
+                                    <button type="submit" class="btn btn-outline-success">Ingresar</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-        @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        </div>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+    </main>
+    <footer class="footer-login" style="font-size: 14.3px; max-height: 100vh!important; height: 10%!important;">
+        <div class="d-flex justify-content-center align-items-center" style="height: 8%;">
+            <div class="row ">
+                <div class="col-12 col-md-3 col-lg-2">
+                    <p><I></I>Sena ADSI</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-8">
+                    <P>Sena ADSI 2023 &#169; Todos los derechos son reservados </P>
+                </div>
+                <div class="col-12 col-md-3 col-lg-2">
+                    <p>V1.1.15373</p>
+                </div>
             </div>
+        </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+    </footer>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+@endsection
--- Proyecto-Fichas-Robinson/resources/views/layouts/guest.blade.php
+++ Proyecto-Fichas-Robinson/resources/views/layouts/guest.blade.php
@@ -1,21 +1,24 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Sena Sistemas de Información</title>
+    <!-- ============================================================== -->
+    <!-- link de bootstrap, Style, fuentes, icons -->
+    <link rel="stylesheet" href="{{url('../../recursos/css/style.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+</head>
+<body>
+@yield('content')
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script></body>
 </html>
