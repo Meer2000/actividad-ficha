f5c80def62786fcfa5d2d7fc0dee393c379bbcef
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-02-08 21:09:49 -0500

    proyecto reelaborado

---
+++ codigo/database/migrations/2023_02_09_003340_create_programas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->bigInteger('fichas_id')->unsigned();
+
+
+            $table->foreign('fichas_id')->references('id')->on('fichas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programas');
+    }
+};
