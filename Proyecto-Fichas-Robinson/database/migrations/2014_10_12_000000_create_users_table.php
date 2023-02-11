0d35401172103d7a23686fda00eb64e80be5741a
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-02-11 12:19:13 -0500

    add features

--- Proyecto-Fichas-Robinson/database/migrations/2014_10_12_000000_create_users_table.php
+++ Proyecto-Fichas-Robinson/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nombre');
             $table->string('apellido');
             $table->string('email')->unique();
-            $table->string('contrasena');
+            $table->string('password');
             $table->timestamps();
         });
     }
--- Proyecto-Fichas-Robinson/resources/views/fichas/create.blade.php
+++ Proyecto-Fichas-Robinson/resources/views/fichas/create.blade.php
@@ -1,41 +1,50 @@
 @extends('layouts.app')
 @section('content2')
-    <div class="row ">
+    <div class="row" style="margin-top:5%">
         <div class="col-md-2">
             <div class="list-group">
-                <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-                    The current button
-                </button>
-                <button type="button" class="list-group-item list-group-item-action">A second item</button>
-                <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+
+                <a href="#" class="list-group-item list-group-item-action">Crear</Crear></a>
+                <a href="#" class="list-group-item list-group-item-action">Listar</a>
             </div>
 
         </div>
 
-            <div class="col-md-8">
-                <form>
+        <div class="col-md-8">
+            <form>
 
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Numero de ficha</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nivel formativo</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">Hacer un foreach</option>
+                </select>
+                 </div>
+                <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">¿Qué tipo de formación es?</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">hacer foreach</option>
+                </select>
+                </div>
+                <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">¿Trimestre actual?</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">hacer foreach</option>
+                </select>
+                </div>
+                <div class="mb-3">
+                        <button type="submit" class="btn btn-primary" style="float:right">Crear</button>
+                </div >
+            </form>
         </div>
+    </div>
+
+
     <div class="col-md-2"></div>
 
 
-</div>
+    </div>
 @endsection
