804fb9601c59a7999d7c8df44632211ff2500af8
Cfparrra7 <cfparra35@misena.edu.co>
2023-02-11 20:23:41 -0500

    Add view aprendices-index Add route web aprendices-index

--- Proyecto-Fichas-Robinson/routes/web.php
+++ Proyecto-Fichas-Robinson/routes/web.php
@@ -26,4 +26,5 @@ Route::middleware([
 Route::get('/dashboard2', function () {;return view('fichas.index');})->name('dashboard2');
 Route::get('/dashboard3', function () {;return view('aprendices.edit');})->name('dashboard2');
 Route::get('/dashboard4', function () {;return view('aprendices.create');})->name('dashboard2');
+Route::get('/dashboard5', function () {;return view('aprendices.index');})->name('dashboard2');
 
