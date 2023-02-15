0796741b66424f94dccadf6092ae16350b79d317
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-02-14 20:16:01 -0500

    table seeder updated

---
+++ Proyecto-Fichas-Robinson/database/seeders/TrimestresTableSeeders.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Trimestre;
+use Illuminate\Database\Seeder;
+
+class TrimestresTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    //public function run()
+    //{   Trimestre::create([
+    //       'nombre'=>'tecnologo']);
+    //}
+}
