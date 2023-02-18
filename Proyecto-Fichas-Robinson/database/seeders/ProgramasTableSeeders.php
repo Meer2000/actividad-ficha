9b147c7be72297b0d10d00d6dd6c025e9f1ba1c4
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-02-18 00:44:22 -0500

    seeder

---
+++ Proyecto-Fichas-Robinson/database/seeders/ProgramasTableSeeders.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Programa;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProgramasTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      /* Programa::create([
+        'nombre'=>'Mantenimiento de equipos'
+        ]);
+        Programa::create([
+            'nombre'=>'ADSO'
+        ]);
+        Programa::create([
+            'nombre'=>'Electricidad industrial'
+        ]);
+        Programa::create([
+            'nombre'=>'Gestion de Redes'
+        ]);
+        Programa::create([
+            'nombre'=>'Diseño Grafico'
+        ]);
+        Programa::create([
+            'nombre'=>'Telecomunicaciones'
+        ]);*/
+    }
+}
