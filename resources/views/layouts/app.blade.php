4813cd8247dff4deab2dbdc138f9232d0036349d
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-03-03 11:57:10 -0500

    Update app.blade.php
    update time alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
 <script type="text/javascript">
     $(document).ready(function () {
 
-        $(".alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+        $(".alert-dismissible").fadeTo(5000, 500).slideUp(500, function(){
             $(".alert-dismissible").alert('close');
         });
 
