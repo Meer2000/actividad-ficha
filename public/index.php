1a4c0475e11ba3d06496d923717d54533ea13ddc
Meer2000 <90801212+Meer2000@users.noreply.github.com>
2023-03-10 19:47:53 -0500

    f

--- public/index.php
+++ public/index.php
@@ -27,7 +27,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| into the script here, so we don't need to manually load our classes.
 |
 */
 
@@ -49,7 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    //$request = Request::capture()
+    $request = Request::capture()
 )->send();
 
-//$kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
